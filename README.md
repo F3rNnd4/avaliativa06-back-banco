3f6d2d263d0cc851008e64d28174466645159945
Fernanda Alves <fernandaalves.tds2.senai@gmail.com>
2025-04-10 13:57:55 -0300

    feat: add prisma database files

---
+++ README.md
@@ -0,0 +1,2 @@
+# Atividade Avaliativa: Desenvolvimento de API Backend com CRUD Completo e Prisma ORM
+
---
+++ src/routes/livroRoutes.js
@@ -0,0 +1,12 @@
+import express from "express";
+import livroController from "../controllers/livroController.js";
+
+const router = express.Router();
+
+router.get("/", livroController.getAll);
+router.get("/:id", livroController.getById);
+router.post("/", livroController.create);
+router.put("/:id", livroController.update);
+router.delete("/:id", livroController.delete);
+
+export default router;
